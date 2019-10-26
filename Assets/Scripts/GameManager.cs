5095004d364c043961d376aaca94c4c0d5e7ca9a
LevOKr <nomengo667@gmail.com>
2019-10-26 14:17:24 -0800

    Za Warudo And movement

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    private void Awake()
+    {
+        //check if instance exist
+        if(instance == null) 
+        {
+            // if not , set instance to this
+            instance = this;
+        }
+        //if it exist but is not this instance
+        else if(instance != this)
+        {
+            //destroy it
+            Destroy(gameObject);
+        }
+        //set this to be not destroyable
+        DontDestroyOnLoad(gameObject);
+    }
+
+}
---
+++ Assets/Scripts/HeroMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroMovement : MonoBehaviour
+{
+
+     float moveSpeed = 10f;
+    void Start()
+    {
+        
+    }
+
+    
+    void FixedUpdate()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveX, 0f, moveZ);
+        GetComponent<Rigidbody>().velocity = movement * moveSpeed;
+    }
+}
